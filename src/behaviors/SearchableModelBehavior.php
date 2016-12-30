66754f6f16853de466a0008f6f705a4fd4ed5a88
Philip Verbist <philip.verbist@gmail.com>
2016-12-30 23:19:00 +0100

    Added SearchableModelBehavior

---
+++ src/behaviors/SearchableModelBehavior.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace verbi\yii2ExtendedActiveRecord\behaviors;
+
+use verbi\yii2Helpers\behaviors\base\Behavior;
+
+/**
+ * @author Philip Verbist <philip.verbist@gmail.com>
+ * @link https://github.com/verbi/yii2-extended-activerecord/
+ * @license https://opensource.org/licenses/GPL-3.0
+ */
+class SearchableModelBehavior extends Behavior {
+
+    protected $fields = [
+        'q' => [
+            'label' => 'Search Term',
+            'options' => [
+                'name' => 'q',
+                'placeholder' => '',
+            ],
+        ],
+    ];
+    protected $values = [];
+    protected $fieldSets = [];
+
+    public function search($data, $formName = null) {
+        $this->owner->load($data, $formName);
+        $this->values = $data;
+        $modelClass = $this->owner->className();
+        $query = $modelClass::find();
+        foreach (array_keys($this->fields) as $name) {
+            if (isset($this->values[$name])) {
+                $query->andWhere('MATCH (' . implode(',', array_slice($this->owner->attributes(), 2)) . ') AGAINST (:' . $name . ' IN NATURAL LANGUAGE MODE)', [':' . $name => $this->values[$name]]);
+            }
+        }
+        return $query;
+    }
+
+    public function getAttributesForSearchForm() {
+        return array_map(function($field) {
+            if (isset($field['options']) && isset($field['options']['name'])) {
+                $field['options']['value'] = isset($this->values[$field['options']['name']]) ? $this->values[$field['options']['name']] : '';
+            }
+            return $field;
+        }, $this->fields);
+    }
+
+    protected function generateWhereCondition($field, $query) {
+        $tableSchema = $this->owner->getTableSchema();
+    }
+}
