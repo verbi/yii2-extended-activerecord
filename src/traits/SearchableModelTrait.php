ad8cceb6c8b1c25aed6d8e1564f23769a3cc4420
Philip Verbist <philip.verbist@gmail.com>
2016-12-30 23:22:40 +0100

    Added SearchableModelTrait

---
+++ src/traits/SearchableModelTrait.php
@@ -0,0 +1,25 @@
+<?php
+namespace verbi\yii2ExtendedActiveRecord\traits;
+
+use verbi\yii2DynamicForms\components\Form;
+use verbi\yii2Helpers\events\GeneralFunctionEvent;
+use yii\web\JsExpression;
+
+trait SearchableModelTrait {
+//    public $q;
+//    
+//    public function search($data, $formName = null) {
+//        
+//    }
+//    
+//    public function getAttributesForSearchForm() {
+//        return [
+//            'q' => [
+//                'type' => 'textInput',
+//                'options' => [
+//                    'placeholder' => 'Title'
+//                ]
+//            ]
+//        ];
+//    }
+}
