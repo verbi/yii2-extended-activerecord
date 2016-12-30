c2adf361eec51cd498416f5fb70b6faf9b4bb601
Philip Verbist <philip.verbist@gmail.com>
2016-12-30 23:21:04 +0100

    Added ElasticSearch ActiveQuery

---
+++ src/elasticsearch/ActiveQuery.php
@@ -0,0 +1,9 @@
+<?php
+namespace verbi\yii2ExtendedActiveRecord\elasticsearch;
+
+/**
+ * @author Philip Verbist <philip.verbist@gmail.com>
+ * @link https://github.com/verbi/yii2-extended-activerecord/
+ * @license https://opensource.org/licenses/GPL-3.0
+ */
+class ActiveQuery extends \yii\elasticsearch\ActiveQuery {}
