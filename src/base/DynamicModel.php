d609bd8b6127f63e1be527755553b3880a5945bf
Philip Verbist <philip.verbist@gmail.com>
2016-12-30 23:17:51 +0100

    Added DynamicModel

---
+++ src/base/DynamicModel.php
@@ -0,0 +1,13 @@
+<?php
+namespace verbi\yii2ExtendedActiveRecord\base;
+
+/**
+ * @author Philip Verbist <philip.verbist@gmail.com>
+ * @link https://github.com/verbi/yii2-extended-activerecord/
+ * @license https://opensource.org/licenses/GPL-3.0
+ */
+class DynamicModel extends \yii\base\DynamicModel
+{
+    use \verbi\yii2Helpers\traits\ComponentTrait;
+    use \verbi\yii2ExtendedActiveRecord\traits\ModelFormTrait;
+}
