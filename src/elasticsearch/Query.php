730b742b4da240a0e1d1d76b9f14486460686376
Philip Verbist <philip.verbist@gmail.com>
2016-12-30 23:21:38 +0100

    Added ElasticSearch Query

---
+++ src/elasticsearch/Query.php
@@ -0,0 +1,9 @@
+<?php
+namespace verbi\yii2ExtendedActiveRecord\db;
+
+/**
+ * @author Philip Verbist <philip.verbist@gmail.com>
+ * @link https://github.com/verbi/yii2-extended-activerecord/
+ * @license https://opensource.org/licenses/GPL-3.0
+ */
+class Query extends \yii\elasticsearch\Query {}
