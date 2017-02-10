1e6dc51327c1065ddeeb786cdb57c2a9b78f31a4
Philip Verbist <philip.verbist@gmail.com>
2017-02-10 19:01:08 +0100

    add rules to UserOwnedModelBehavior

--- src/behaviors/UserOwnedModelBehavior.php
+++ src/behaviors/UserOwnedModelBehavior.php
@@ -1,17 +1,21 @@
 <?php
 
 namespace verbi\yii2ExtendedActiveRecord\behaviors;
+
+use Yii;
 use verbi\yii2Helpers\behaviors\base\Behavior;
 use verbi\yii2Helpers\events\GeneralFunctionEvent;
 use verbi\yii2Helpers\base\ArrayObject;
 use yii\validators\DefaultValueValidator;
 use yii\validators\Validator;
+
 /**
  * @author Philip Verbist <philip.verbist@gmail.com>
  * @link https://github.com/verbi/yii2-extended-activerecord/
  * @license https://opensource.org/licenses/GPL-3.0
  */
 class UserOwnedModelBehavior extends Behavior {
+
     public function events() {
         $ownerClass = $this->owner->className();
         return [
@@ -19,42 +23,55 @@ class UserOwnedModelBehavior extends Behavior {
             $ownerClass::$EVENT_AFTER_CREATE_VALIDATORS => 'afterCreateValidators',
         ];
     }
-    
+
     public function afterGetFormAttributes(GeneralFunctionEvent $event) {
-        if($this->owner && isset($event->params['attributes']) && array_key_exists('owner_id',$event->params['attributes'])) {
+        if ($this->owner && isset($event->params['attributes']) && array_key_exists('owner_id', $event->params['attributes'])) {
             $params = $event->params;
             unset($params['attributes']['owner_id']);
             $event->setparams($params);
             $event->setReturnValue($params['attributes']);
         }
     }
-    
+
     public function afterCreateValidators(GeneralFunctionEvent $event) {
-        if($this->owner) {
-            if(
-                !\Yii::$app->user->isGuest
-                && isset($event->params['validators']) 
-                && $event->params['validators'] instanceof ArrayObject
+        if ($this->owner) {
+            if (
+                    !\Yii::$app->user->isGuest && isset($event->params['validators']) && $event->params['validators'] instanceof ArrayObject
             ) {
-                if($this->owner->hasAttribute('owner_id')) {
-                    foreach($event->params['validators'] as $validator) {
-                        if(
-                                $validator instanceof DefaultValueValidator
-                                && in_array('owner_id',$validator->attributes)) {
+                if ($this->owner->hasAttribute('owner_id')) {
+                    foreach ($event->params['validators'] as $validator) {
+                        if (
+                                $validator instanceof DefaultValueValidator && in_array('owner_id', $validator->attributes)) {
                             return;
                         }
                     }
-                $validator = Validator::createValidator(
-                        'default',
-                        $this->owner,
-                        ['owner_id'],
-                        ['value' => \Yii::$app->user->identity->getId()]
+                    $validator = Validator::createValidator(
+                                    'default', $this->owner, ['owner_id'], ['value' => \Yii::$app->user->identity->getId()]
                     );
-                $validators = $event->params['validators'];
-                $validators->prepend($validator);
-                $event->setReturnValue($validators);
-                return;
+                    $validators = $event->params['validators'];
+                    $validators->prepend($validator);
+                    $event->setReturnValue($validators);
+                    return;
                 }
+            }
+        }
+    }
+
+    public function addAuthRules($controller) {
+        $auth = Yii::$app->authManager;
+
+        if($auth) {
+            // add the rule
+            $rule = new \verbi\yii2ExtendedActiveRecord\rbac\UserOwnedRule;
+            $auth->add($rule);
+            // add the "updateOwnPost" permission and associate the rule with it.
+            foreach(array_keys($controller->getActions()) as $actionId){
+                $permission = $auth->createPermission($this->owner->className().'-'.$actionId.'-Own');
+                $permission->description = 'Update own '.$this->owner->className();
+                $permission->ruleName = $rule->name;
+                $auth->add($permission);
+                
+//                $auth->addChild($permission, $updatePost);
             }
         }
     }
